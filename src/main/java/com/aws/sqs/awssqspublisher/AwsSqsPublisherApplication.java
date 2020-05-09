6134b2b92f1d29dd8dd1919fdcde6689c2ab4c76
Joaquim Francisco Vieira Junior <joaquimjuniorsf@gmail.com>
2020-05-09 14:41:26 -0300

    first commit

---
+++ src/main/java/com/aws/sqs/awssqspublisher/AwsSqsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.aws.sqs.awssqspublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/sqs/awssqspublisher/AwsSqsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.sqs.awssqspublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
