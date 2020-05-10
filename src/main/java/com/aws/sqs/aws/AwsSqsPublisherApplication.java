20ad73ac9640dbfb2e79482876f9f229a5c37e9e
Joaquim Francisco Vieira Junior <joaquimjuniorsf@gmail.com>
2020-05-09 23:29:24 -0300

    created a message producer

--- src/main/java/com/aws/sqs/aws/AwsSqsPublisherApplication.java
+++ src/main/java/com/aws/sqs/aws/AwsSqsPublisherApplication.java
@@ -1,9 +1,12 @@
-package com.aws.sqs.awssqspublisher;
+package com.aws.sqs.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ContextStackAutoConfiguration.class)
+@EnableScheduling
 public class AwsSqsPublisherApplication {
 
 	public static void main(String[] args) {
